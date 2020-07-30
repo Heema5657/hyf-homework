0649cef6eccb3cc147c2866155a196b283533fb4
Heema5657 <66559310+Heema5657@users.noreply.github.com>
2020-07-30 19:55:58 +0200

    Update housey.js

--- javascript/javascript1/week1/housey.js
+++ javascript/javascript1/week1/housey.js
@@ -1,7 +1,7 @@
-let volumeInMeters =800;
-let houseCost = 2500000;
-let gardenSizeInM2 =100;
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+const volumeInMeters =800;
+const houseCost = 2500000;
+const gardenSizeInM2 =100;
+const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 let housey;
 if(houseCost >= housePrice){
     housey = 'Peters House is Pricey';

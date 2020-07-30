0c7684bc0071f46ae905db894824d6d516a5ae10
Heema5657 <66559310+Heema5657@users.noreply.github.com>
2020-07-30 19:53:12 +0200

    Update ez.js

--- javascript/javascript1/week1/ez.js
+++ javascript/javascript1/week1/ez.js
@@ -1,5 +1,5 @@
-let firstWords =["easy","complicated","strong","boring","hard","interesting","good","bad","bold","italics","regular"];
-let secondWords =["red","blue","green","yellow","black","white","violet","indigo","airplane","telephone",];
+const firstWords =["easy","complicated","strong","boring","hard","interesting","good","bad","bold","italics","regular"];
+const secondWords =["red","blue","green","yellow","black","white","violet","indigo","airplane","telephone",];
 
 let startupName;
 startupName = firstWords[Math.floor(Math.random() * 10)] + " " + secondWords[Math.floor(Math.random() * 10)],
